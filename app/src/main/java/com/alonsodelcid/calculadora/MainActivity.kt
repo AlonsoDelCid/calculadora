95c1c3a164b49b5a9127bfb5cba9b09f9180eb2c
Seth Díaz <seth261099@gmail.com>
2021-10-14 15:37:39 -0500

    Actualización hipotenusa

--- app/src/main/java/com/alonsodelcid/calculadora/MainActivity.kt
+++ app/src/main/java/com/alonsodelcid/calculadora/MainActivity.kt
@@ -2,90 +2,32 @@ package com.alonsodelcid.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.roundToLong
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
-    private var num1 = 0.0
-    private var num2 = 0.0
-    private var operacion = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultadoText.text = "0"
-        operacion = SIN_OPERACION
-
-        unoBtn.setOnClickListener { numberPressed("1") }
-        dosBtn.setOnClickListener { numberPressed("2") }
-        tresBtn.setOnClickListener { numberPressed("3") }
-        cuatroBtn.setOnClickListener { numberPressed("4") }
-        cincoBtn.setOnClickListener { numberPressed("5") }
-        seisBtn.setOnClickListener { numberPressed("6") }
-        sieteBtn.setOnClickListener { numberPressed("7") }
-        ochoBtn.setOnClickListener { numberPressed("8") }
-        nueveBtn.setOnClickListener { numberPressed("9") }
-        ceroBtn.setOnClickListener { numberPressed("0") }
-        puntoBtn.setOnClickListener { numberPressed(".") }
-
-        clearBtn.setOnClickListener { resetAll() }
-
-        sumaBtn.setOnClickListener { operationPressed(SUMA) }
-        restaBtn.setOnClickListener { operationPressed(RESTA) }
-        multiplicarBtn.setOnClickListener { operationPressed(MULTIPLICACION) }
-        divisionBtn.setOnClickListener { operationPressed(DIVISION) }
-
-        igualBtn.setOnClickListener { resolvePressed() }
+        btnCalcular.setOnClickListener{ calcular()}
     }
 
-    private fun numberPressed(num: String){
-        if(resultadoText.text == "0" && num != ".") {
-            resultadoText.text = "$num"
-        } else {
-            resultadoText.text = "${resultadoText.text}$num"
+    private fun calcular() {
+        val num1 = numero1.text.toString().toDoubleOrNull()
+        val num2 = numero2.text.toString().toDoubleOrNull()
+
+        if((num1 == null) or (num2 == null)){
+            Toast.makeText(this, "Los campos no pueden estar vacíos", Toast.LENGTH_LONG).show()
         }
-
-        if(operacion == SIN_OPERACION){
-            num1 = resultadoText.text.toString().toDouble()
-        } else {
-            num2 = resultadoText.text.toString().toDouble()
+        else{
+            var hipo: Double = num1!!.toDouble().pow(2) + num2!!.toDouble().pow(2);
+            hipo = sqrt(hipo)
+            Toast.makeText(this, "La medida de la hipotenusa es ".plus(hipo.toString()), Toast.LENGTH_LONG).show()
+            numero1.text = null
+            numero2.text = null
         }
-    }
-
-    private fun operationPressed(operacion: Int){
-        this.operacion = operacion
-        num1 = resultadoText.text.toString().toDouble()
-
-        resultadoText.text = "0"
-    }
-
-    private fun resolvePressed(){
-
-        val result = when(operacion) {
-            SUMA -> num1 + num2
-            RESTA -> num1 - num2
-            MULTIPLICACION -> num1 * num2
-            DIVISION -> num1 / num2
-            else -> 0
-        }
-
-        num1 = result as Double
-
-        resultadoText.text = if("$result".endsWith(".0")) { "$result".replace(".0","") } else { "%.2f".format(result) }
-    }
-
-    private fun resetAll(){
-        resultadoText.text = "0"
-        num1 = 0.0
-        num2 = 0.0
-    }
-
-    companion object {
-        const val SUMA = 1
-        const val RESTA = 2
-        const val MULTIPLICACION = 3
-        const val DIVISION = 4
-        const val SIN_OPERACION = 0
     }
 }
